8f10a04cc4c9ac1d25ba53f4fcd71336e6502e38
RubenTT91 <ruben.toro1@gmail.com>
2024-10-20 12:48:27 -0500

    se ajustan los nombres de los métodos

--- contracts/SistemaDeRecompensas.sol
+++ contracts/SistemaDeRecompensas.sol
@@ -16,17 +16,17 @@ contract SistemaDeRecompensas{
         UmbralParaRecompensa = _UmbralParaRecompensa;
     }
 
-    event PuntosAcumulados(uint256 puntosUsuario); //agregamos un evento para almacenar la traza de puntos acumulados
+    event puntosAcumulados(uint256 puntosUsuario); //agregamos un evento para almacenar la traza de puntos acumulados
 
 
-      function AcumularPuntos(uint256 _sumarPuntos ) public  {
+      function acumularPuntos(uint256 _sumarPuntos ) public  {
         require(_sumarPuntos>0,"Los puntos deben ser superior a cero.");        //Exigimos un valor superior a 0. 
         require(puntosUsuario<UmbralParaRecompensa, "Usted ha completado el umbral, no puede acumular mas puntos");  //validación para evitar acumular más puntos.
         puntosUsuario += _sumarPuntos;
-        emit PuntosAcumulados(puntosUsuario);  //se activa el evento
+        emit puntosAcumulados(puntosUsuario);  //se activa el evento
       } 
 
-      function ConsultarEstadoDeRecompensa() public view returns(uint256, uint256, bool){
+      function consultarEstadoDeRecompensa() public view returns(uint256, uint256, bool){
         /*ConsultarEstadoDeRecompensa: Ofrece a los usuarios la posibilidad de consultar el umbral establecido para las recompensas,
          cuántos puntos han acumulado y si han alcanzado el umbral para obtener una recompensa
         */
