6deada72b3003495b11828a0133a889a401f8a0b
RubenTT91 <ruben.toro1@gmail.com>
2024-10-19 23:00:16 -0500

    se trabaja sobre sistemas de suscripción y se crean las funciones y eventos

--- contracts/SistemaDeSuscripcion.sol
+++ contracts/SistemaDeSuscripcion.sol
@@ -2,6 +2,22 @@
 pragma solidity >0.8.0 <=0.9.0;
 
 contract SistemaDeSuscripcion{
-    
+
+    uint256 costoSuscripcion;
+    uint256 descuentoAplicado;
+    uint256 costoMensual;
+    uint256 tasaDescuento;
+
+constructor(uint256 _tasaDescuento) {
+    tasaDescuento = _tasaDescuento;
+}
+
+    event ObtenerCostos(uint256 costoSuscripcion, uint256 descuentoAplicado, uint256 costoMensual);
+
+    function establecerSuscripcionYCalcularCosto() public {
+        
+        emit ObtenerCostos(costoSuscripcion,descuentoAplicado,costoMensual);
+    }
+
     
 }
