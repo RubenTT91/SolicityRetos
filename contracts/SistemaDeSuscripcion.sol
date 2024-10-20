43377ec53c840101af488e3f96698d563213bc56
RubenTT91 <ruben.toro1@gmail.com>
2024-10-20 12:21:02 -0500

    Se completa el reto 3 Suscripción

--- contracts/SistemaDeSuscripcion.sol
+++ contracts/SistemaDeSuscripcion.sol
@@ -1,23 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.8.0 <=0.9.0;
+pragma solidity ^0.8.0;
 
-contract SistemaDeSuscripcion{
+contract SistemaDeSuscripcion {
+    uint256 public tasaDescuento;
+    uint256 public costoMensual;
+    address public administrador;
 
-    uint256 costoSuscripcion;
-    uint256 descuentoAplicado;
-    uint256 costoMensual;
-    uint256 tasaDescuento;
+    event CostosDeSuscripcion(uint256 costoMensual, uint256 costoConDescuento);
 
-constructor(uint256 _tasaDescuento) {
-    tasaDescuento = _tasaDescuento;
-}
-
-    event ObtenerCostos(uint256 costoSuscripcion, uint256 descuentoAplicado, uint256 costoMensual);
-
-    function establecerSuscripcionYCalcularCosto() public {
-        
-        emit ObtenerCostos(costoSuscripcion,descuentoAplicado,costoMensual);
+    constructor(uint256 _tasaDescuento) {
+        tasaDescuento = _tasaDescuento;
+        administrador = msg.sender;
     }
 
-    
+    modifier soloAdministrador() {
+        require(msg.sender == administrador, "Solo el administrador puede ejecutar esta funcion");
+        _;
+    }
+
+    function establecerSuscripcionYCalcularCosto(uint256 _costoMensual) public soloAdministrador {
+        costoMensual = _costoMensual;
+        uint256 costoConDescuento = calcularCostoConDescuento(_costoMensual);
+        emit CostosDeSuscripcion(_costoMensual, costoConDescuento);
+    }
+
+    function calcularCostoConDescuento(uint256 _costoMensual) internal view returns (uint256) {
+        return _costoMensual - (_costoMensual * tasaDescuento / 100);
+    }
+
+    function obtenerCostos() public view returns (uint256, uint256) {
+        uint256 costoConDescuento = calcularCostoConDescuento(costoMensual);
+        return (costoMensual, costoConDescuento);
+    }
 }
